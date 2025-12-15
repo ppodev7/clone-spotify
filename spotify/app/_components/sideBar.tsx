18cb2a7387ec4392c2d9df087a129bffb972df5c
Pedro Fidelis <pfidelis038@gmail.com>
2025-12-14 21:59:13 -0300

    feat: pagina principal sendo alterada

--- spotify/app/_components/sideBar.tsx
+++ spotify/app/_components/sideBar.tsx
@@ -1,10 +1,42 @@
+import { Plus } from 'lucide-react'
+import { Search } from 'lucide-react'
+import { List } from 'lucide-react'
+
 export function SideBar() {
   return (
-    <aside className="w-72 bg-zinc-900 p-6 rounded-lg ">
+    <aside className="w-96 bg-zinc-900 p-6 rounded-lg items-center">
       <nav className="space-y-5">
-        <a href="" className="flex font-semibold text-zinc-100">
+        <div className="flex items-center justify-between">
+          <a href="" className="font-semibold text-zinc-100">
             Sua Biblioteca
-        </a>
+          </a>
+          <button className="flex items-center gap-2 text-sm font-semibold bg-zinc-800 px-4 rounded-4xl p-2 text-zinc-200 hover:text-zinc-100">
+            <Plus />
+            Criar
+          </button>
+        </div>
+
+        <div className='flex items-center gap-4 text-sm font-semibold '>
+          <button className='bg-zinc-700 p-2 rounded-4xl px-3'>
+            Playlists
+          </button>
+
+          <button className='bg-zinc-700 p-2 rounded-4xl px-3'>
+            Artistas
+          </button>
+
+          <button className='bg-zinc-700 p-2 rounded-4xl px-3'>
+            Álbuns
+          </button>
+        </div>
+
+        <div className='flex items-center justify-between'>
+          <Search className='w-5 h-5 text-zinc-400 hover:text-zinc-100 cursor-pointer transition-colors'/>
+          <div className='flex items-center gap-2'>
+            <span className='text-sm font-semibold text-zinc-400 hover:text-zinc-100 cursor-pointer transition-colors'>Recentes</span>
+            <List className='w-5 h-5 text-zinc-400 hover:text-zinc-100 cursor-pointer transition-colors'/>
+          </div>
+        </div>
       </nav>
     </aside>
   )
--- spotify/app/page.tsx
+++ spotify/app/page.tsx
@@ -3,19 +3,33 @@ import { Navbar } from "./_components/navbar";
 
 export default function Home() {
   return (
-    
+
     <div className="h-screen flex flex-col">
-      
+
       <div className="h-14 bg-zinc-950 p-6 flex items-center">
         <Navbar />
       </div>
-      
-      
-      
+
+
+
       <div className="flex flex-1 p-2 gap-2">
         <SideBar />
         <main className="flex-1 p-6 bg-zinc-900 rounded-lg">
-          Main Content
+          <nav>
+            <div className="flex gap-4 font-semibold">
+              <button className="bg-zinc-600 p-2 rounded-4xl px-4">
+                Tudo
+              </button>
+
+              <button className="bg-zinc-600 p-2 rounded-4xl px-5">
+                Músicas
+              </button>
+
+              <button className="bg-zinc-600 p-2 rounded-4xl px-5">
+                Podcasts
+              </button>
+            </div>
+          </nav>
         </main>
       </div>
     </div>
